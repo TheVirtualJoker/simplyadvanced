500692cf810bdd98508399b622f9f0a2fdbf9441
VirtualJoker <brandenricci@gmail.com>
2017-11-28 16:17:51 -0500

    Joker Commit #2: Fixed main class, Organized references, Updated mcmod.info
    Bunch of new changes. Updated and fixed repository

---
+++ src/main/java/com/joker/simplyadvanced/Main.java
@@ -0,0 +1,29 @@
+package com.joker.simplyadvanced;
+
+import com.joker.simplyadvanced.lib.References;
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = References.MODID, version = References.VERSION)
+public class Main
+{
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+
+    }
+}
---
+++ src/main/java/com/joker/simplyadvanced/lib/References.java
@@ -0,0 +1,6 @@
+package com.joker.simplyadvanced.lib;
+
+public class References {
+    public static final String VERSION = "0.0.1";
+    public static final String MODID = "simplyadvanced";
+}
