2c7b52024bed08b46392ced4ab6f413ad25f34d6
VirtualJoker <brandenricci@gmail.com>
2017-11-29 18:48:28 -0500

    Joker Commit #14: Added high temperature fuel

---
+++ src/main/java/com/joker/simplyadvanced/common/items/fuels/ItemHighTempFuel.java
@@ -0,0 +1,12 @@
+package com.joker.simplyadvanced.common.items.fuels;
+
+import com.joker.simplyadvanced.common.utils.CreativeUtil;
+import net.minecraft.item.Item;
+
+public class ItemHighTempFuel extends Item {
+    public ItemHighTempFuel() {
+        setUnlocalizedName("high_temp_fuel");
+        setRegistryName("high_temp_fuel");
+        setCreativeTab(CreativeUtil.TAB);
+    }
+}
