4c6f2117d30c5c374da52cb2d8529801a3af4cb9
brainsynder498 <starwars4393@hotmail.com>
2017-11-29 11:38:52 -0500

    Brian Commit #8
    Stared work on the TileEntity for the Kiln

--- src/main/java/com/joker/simplyadvanced/common/tiles/TileEntityKiln.java
+++ src/main/java/com/joker/simplyadvanced/common/tiles/TileEntityKiln.java
@@ -1,6 +1,9 @@
 package com.joker.simplyadvanced.common.tiles;
 
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.NonNullList;
 
 public class TileEntityKiln extends TileEntity {
+    private NonNullList<ItemStack> inventory = NonNullList.withSize(1, ItemStack.EMPTY);
 }
